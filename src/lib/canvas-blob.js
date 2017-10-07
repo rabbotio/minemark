fad4a4405c93fe524bfbc362f01dad52ab5d42e7
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-07 17:52:28 +0700

    refactor(canvas): Change canvas to blob lib

--- src/lib/canvas-blob.js
+++ src/lib/canvas-blob.js
@@ -1,188 +1,110 @@
-/* canvas-toBlob.js
- * A canvas.toBlob() implementation.
- * 2016-05-26
+/*
+ * JavaScript Canvas to Blob
+ * https://github.com/blueimp/JavaScript-Canvas-to-Blob
  *
- * By Eli Grey, http://eligrey.com and Devin Samarin, https://github.com/eboyjr
- * License: MIT
- *   See https://github.com/eligrey/canvas-toBlob.js/blob/master/LICENSE.md
+ * Copyright 2012, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ *
+ * Based on stackoverflow user Stoive's code snippet:
+ * http://stackoverflow.com/q/4998908
  */
 
-/* global self */
-/* jslint bitwise: true, regexp: true, confusion: true, es5: true, vars: true, white: true,
-  plusplus: true */
+/* global atob, Blob, define */
 
-/*! @source http://purl.eligrey.com/github/canvas-toBlob.js/blob/master/canvas-toBlob.js */
-
-;(function (view) {
-  var Uint8Array = view.Uint8Array,
-    HTMLCanvasElement = view.HTMLCanvasElement,
-    canvas_proto = HTMLCanvasElement && HTMLCanvasElement.prototype,
-    is_base64_regex = /\s*;\s*base64\s*(?:;|$)/i,
-    to_data_url = 'toDataURL',
-    base64_ranks,
-    decode_base64 = function (base64) {
-      var len = base64.length, buffer = new Uint8Array((len / 4 * 3) | 0), i = 0, outptr = 0, last = [0, 0], state = 0, save = 0, rank, code, undef
-
-      while (len--) {
-        code = base64.charCodeAt(i++)
-        rank = base64_ranks[code - 43]
-        if (rank !== 255 && rank !== undef) {
-          last[1] = last[0]
-          last[0] = code
-          save = (save << 6) | rank
-          state++
-          if (state === 4) {
-            buffer[outptr++] = save >>> 16
-            if (last[1] !== 61 /* padding character */) {
-              buffer[outptr++] = save >>> 8
-            }
-            if (last[0] !== 61 /* padding character */) {
-              buffer[outptr++] = save
-            }
-            state = 0
-          }
-        }
+;(function (window) {
+  'use strict'
+  var CanvasPrototype = window.HTMLCanvasElement && window.HTMLCanvasElement.prototype
+  var hasBlobConstructor =
+    window.Blob &&
+    (function () {
+      try {
+        return Boolean(new Blob())
+      } catch (e) {
+        return false
       }
-      // 2/3 chance there's going to be some null bytes at the end, but that
-      // doesn't really matter with most image formats.
-      // If it somehow matters for you, truncate the buffer up outptr.
-      return buffer
+    })()
+  var hasArrayBufferViewSupport =
+    hasBlobConstructor &&
+    window.Uint8Array &&
+    (function () {
+      try {
+        return new Blob([new Uint8Array(100)]).size === 100
+      } catch (e) {
+        return false
+      }
+    })()
+  var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder
+  var dataURIPattern = /^data:((.*?)(;charset=.*?)?)(;base64)?,/
+  var dataURLtoBlob =
+    (hasBlobConstructor || BlobBuilder) &&
+    window.atob &&
+    window.ArrayBuffer &&
+    window.Uint8Array &&
+    function (dataURI) {
+      var matches, mediaType, isBase64, dataString, byteString, arrayBuffer, intArray, i, bb
+      // Parse the dataURI components as per RFC 2397
+      matches = dataURI.match(dataURIPattern)
+      if (!matches) {
+        throw new Error('invalid data URI')
+      }
+      // Default to text/plain;charset=US-ASCII
+      mediaType = matches[2] ? matches[1] : 'text/plain' + (matches[3] || ';charset=US-ASCII')
+      isBase64 = !!matches[4]
+      dataString = dataURI.slice(matches[0].length)
+      if (isBase64) {
+        // Convert base64 to raw binary data held in a string:
+        byteString = atob(dataString)
+      } else {
+        // Convert base64/URLEncoded data component to raw binary:
+        byteString = decodeURIComponent(dataString)
+      }
+      // Write the bytes of the string to an ArrayBuffer:
+      arrayBuffer = new ArrayBuffer(byteString.length)
+      intArray = new Uint8Array(arrayBuffer)
+      for (i = 0; i < byteString.length; i += 1) {
+        intArray[i] = byteString.charCodeAt(i)
+      }
+      // Write the ArrayBuffer (or ArrayBufferView) to a blob:
+      if (hasBlobConstructor) {
+        return new Blob([hasArrayBufferViewSupport ? intArray : arrayBuffer], {
+          type: mediaType
+        })
+      }
+      bb = new BlobBuilder()
+      bb.append(arrayBuffer)
+      return bb.getBlob(mediaType)
     }
-  if (Uint8Array) {
-    base64_ranks = new Uint8Array([
-      62,
-      -1,
-      -1,
-      -1,
-      63,
-      52,
-      53,
-      54,
-      55,
-      56,
-      57,
-      58,
-      59,
-      60,
-      61,
-      -1,
-      -1,
-      -1,
-      0,
-      -1,
-      -1,
-      -1,
-      0,
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      23,
-      24,
-      25,
-      -1,
-      -1,
-      -1,
-      -1,
-      -1,
-      -1,
-      26,
-      27,
-      28,
-      29,
-      30,
-      31,
-      32,
-      33,
-      34,
-      35,
-      36,
-      37,
-      38,
-      39,
-      40,
-      41,
-      42,
-      43,
-      44,
-      45,
-      46,
-      47,
-      48,
-      49,
-      50,
-      51
-    ])
+  if (window.HTMLCanvasElement && !CanvasPrototype.toBlob) {
+    if (CanvasPrototype.mozGetAsFile) {
+      CanvasPrototype.toBlob = function (callback, type, quality) {
+        var self = this
+        setTimeout(function () {
+          if (quality && CanvasPrototype.toDataURL && dataURLtoBlob) {
+            callback(dataURLtoBlob(self.toDataURL(type, quality)))
+          } else {
+            callback(self.mozGetAsFile('blob', type))
+          }
+        })
+      }
+    } else if (CanvasPrototype.toDataURL && dataURLtoBlob) {
+      CanvasPrototype.toBlob = function (callback, type, quality) {
+        var self = this
+        setTimeout(function () {
+          callback(dataURLtoBlob(self.toDataURL(type, quality)))
+        })
+      }
+    }
   }
-  if (HTMLCanvasElement && (!canvas_proto.toBlob || !canvas_proto.toBlobHD)) {
-    if (!canvas_proto.toBlob) {
-      canvas_proto.toBlob = function (callback, type /*, ...args */) {
-        if (!type) {
-          type = 'image/png'
-        }
-        if (this.mozGetAsFile) {
-          callback(this.mozGetAsFile('canvas', type))
-          return
-        }
-        if (this.msToBlob && /^\s*image\/png\s*(?:$|;)/i.test(type)) {
-          callback(this.msToBlob())
-          return
-        }
-
-        var args = Array.prototype.slice.call(arguments, 1),
-          dataURI = this[to_data_url].apply(this, args),
-          header_end = dataURI.indexOf(','),
-          data = dataURI.substring(header_end + 1),
-          is_base64 = is_base64_regex.test(dataURI.substring(0, header_end)),
-          blob
-        if (Blob.fake) {
-          // no reason to decode a data: URI that's just going to become a data URI again
-          blob = new Blob()
-          if (is_base64) {
-            blob.encoding = 'base64'
-          } else {
-            blob.encoding = 'URI'
-          }
-          blob.data = data
-          blob.size = data.length
-        } else if (Uint8Array) {
-          if (is_base64) {
-            blob = new Blob([decode_base64(data)], { type: type })
-          } else {
-            blob = new Blob([decodeURIComponent(data)], { type: type })
-          }
-        }
-        callback(blob)
-      }
-    }
-
-    if (!canvas_proto.toBlobHD && canvas_proto.toDataURLHD) {
-      canvas_proto.toBlobHD = function () {
-        to_data_url = 'toDataURLHD'
-        var blob = this.toBlob()
-        to_data_url = 'toDataURL'
-        return blob
-      }
-    } else {
-      canvas_proto.toBlobHD = canvas_proto.toBlob
-    }
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return dataURLtoBlob
+    })
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = dataURLtoBlob
+  } else {
+    window.dataURLtoBlob = dataURLtoBlob
   }
 })(window)
