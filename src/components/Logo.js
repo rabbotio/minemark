3964396be43708d2768947bb712bb2686ba4ba8f
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-07 17:52:46 +0700

    style(logo): Add emboss effect

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,17 @@
 import React from 'react'
+// Styles
+import styled from 'styled-components'
+
+const Prez = styled.pre`
+color: #ffffff;
+text-shadow: -0.5px -0.5px 0.5px #000;
+`
 
 const Logo = () => (
-  <pre>{` _____ _           _____         _   
+  <Prez>{` _____ _           _____         _   
 |     |_|_____ ___|     |___ ___| |_ 
 | | | | |     | -_| | | | .'|  _| '_|
-|_|_|_|_|_|_|_|___|_|_|_|__,|_| |_,_|`}</pre>
+|_|_|_|_|_|_|_|___|_|_|_|__,|_| |_,_|`}</Prez>
 )
 
 export default Logo
