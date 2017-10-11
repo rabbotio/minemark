3917a6364b16c49300e8c559406a24dd68d36864
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-11 15:55:58 +0700

    style(logo): Use shadow as int

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 const Prez = styled.pre`
 color: #ffffff;
-text-shadow: -0.5px -0.5px 0.5px #000;
+text-shadow: -1px -1px 1px #666666;
 `
 
 const Logo = () => (
