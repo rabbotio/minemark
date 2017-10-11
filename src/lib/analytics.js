2a1d73e65c3ebe5feee1e0355aa168bd8bfd852b
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-11 14:28:14 +0700

    fix(analytics): Fix gtag

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -3,28 +3,9 @@ import { name, version } from '../../package.json'
 
 const _isDebug = process.env.NODE_ENV !== 'production'
 const GA_TRACKING_ID = 'UA-107529694-1'
+
 let app_name = `${name}-${version}`
-
-const gtag = (...args) => {
-  if (!window.dataLayer) {
-    console.warn('gtag not init yet:', ...args)
-    return
-  }
-  _isDebug && console.info('gtag push : ', args)
-  window.dataLayer.push(...args)
-}
-
-export const initGA = (appName = app_name) =>
-  new Promise(resolve =>
-    loadScript(`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`, () => {
-      _isDebug && console.info('gtag init')
-      window.dataLayer = window.dataLayer || []
-      console.log('window.dataLayer:', window.dataLayer)
-      gtag('js', new Date())
-      trackPageView()
-    })
-  ).catch(console.warn)
-
+const gtag = window.gtag || (() => {})
 export const trackPageView = (options = {}) => gtag('config', GA_TRACKING_ID, options)
 
 export const trackScreen = screen_name =>
