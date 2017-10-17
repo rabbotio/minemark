0a6cb8a9344c5b9275821e405aa3803232bf4b69
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-17 17:49:12 +0700

    feat(share): Working user_timeline but failed update

---
+++ webtask/oauth/oauth.ts
@@ -0,0 +1,59 @@
+const twitterConsumerKey = 'RlLbTDcVvvDjKsiygbwuf4cTv';
+const twitterConsumerSecret = 'y4JAKA0evhrvBgixnXrbPLKLatnfjkJtPi3Md8hgLuTxiDlvEu';
+
+const willGetOAuthAccessToken = async (twitterConsumerKey, twitterConsumerSecret) => new Promise<string>((resolve, reject) => {
+  const { OAuth2 } = require('oauth');
+  const oauth2 = new OAuth2(
+    twitterConsumerKey,
+    twitterConsumerSecret,
+    'https://api.twitter.com/',
+    null,
+    'oauth2/token',
+    null);
+
+  oauth2.getOAuthAccessToken(
+    '',
+    { 'grant_type': 'client_credentials' },
+    (e, access_token, refresh_token, results) => {
+      if (!access_token) {
+        return reject(new Error(`${results}, ${JSON.stringify(e)}`))
+      }
+
+      return resolve(access_token);
+    });
+})
+
+// Twitter API
+require('isomorphic-fetch')
+const option = token => ({
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
+
+const user_timeline = (token, screen_name) =>
+  fetch(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${screen_name}`, option(token))
+
+const update = (token, status) =>
+  fetch(`https://api.twitter.com/1.1/statuses/update.json?status=${status}`, option(token))
+
+// Express
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) =>
+  willGetOAuthAccessToken(twitterConsumerKey, twitterConsumerSecret)
+    .then(access_token => {
+      // update(access_token, 'https://rabbot.io/minemark')
+      user_timeline(access_token, 'katopz')
+        .then(response => response.json()
+          .then(json => res.send(json)))
+        .catch((err) => console.error(err.message));
+    })
+    .catch((err) => console.error(err.message))
+)
+
+
+// Export
+const WebTask = require('webtask-tools')
+module.exports = WebTask.fromExpress(app)
